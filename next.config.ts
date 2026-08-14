28a4a69e6cd275961a9de3f21b761663aadd853c
cargofe2 <CARGOFE2@gmail.com/>
2026-08-14 00:36:56 -0400

    restore: next.config.ts estado actual sin headers

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 // next.config.ts
 import type { NextConfig } from "next";
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -8,4 +9,5 @@ const nextConfig: NextConfig = {
     ],
   },
 };
+
 export default nextConfig;
